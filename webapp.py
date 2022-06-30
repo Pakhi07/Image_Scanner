272a97c8c90abf6ec8bfcaac36e32be075c784a6
Pakhi07 <92666755+Pakhi07@users.noreply.github.com>
2022-06-30 21:21:35 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,11 +5,10 @@ from PIL import Image
 import numpy as np
 import re   
 import pyrebase
-import datetime as datetime
 
-    
-# st.sidebar.title("Navigation")
-# options=st.sidebar.radio('Pages', options=['Main','Sign in','History'])
+
+st.sidebar.title("Navigation")
+options=st.sidebar.radio('Pages', options=['Main','History'])
 
 firebaseConfig = {
   'apiKey': "AIzaSyCZlxwhnALu8BbUJLxokNtIciQ2jraht0o",
@@ -27,14 +26,12 @@ auth = firebase.auth()
 
 db = firebase.database()
 storage = firebase.storage()
+
 @st.cache
 def load_model():
     reader=ocr.Reader(['en'],model_storage_directory='.')
     return reader
 
-# reader=load_model()  
-
-global img
 def main_function():
     reader=load_model()  
 
@@ -50,13 +47,13 @@ def main_function():
             result=reader.readtext(np.array(input_image))
             for text in result:
                 result_text.append(text[1])
-            # st.write(result_text)
-            # print(result_text)
+            st.write(result_text)
+
         st.success("here you go!")
     else:
         st.write("Upload an image")
     
-    db.child("results").push(result_text)
+    
 
     col1, col2, col3 = st.columns((1,1,1))
 
@@ -68,9 +65,12 @@ def main_function():
             with st.spinner("Loading"):
                 if(re.search(regex,email)):   
                     st.write(email)  
+                    db.child("Email").push(email)
+
 
         for i in range(len(result_text)):
             check_mail(result_text[i])
+
 
     with col2:
         st.markdown('Mobile')
@@ -78,68 +78,53 @@ def main_function():
         def check_num(num):   
             with st.spinner("Loading"):
                 if(re.search(regex,num)):   
-                    st.write(num)  
+                    st.write(num)
+                    db.child("Number").push(num)
+
 
         for i in range(len(result_text)):
             check_num(result_text[i])
+
             
     with col3:
         st.markdown('Web links')
-        regex="\Ahttps"
+        regex="\Ahttp"
         def check_url(web):   
             with st.spinner("Loading"):
                 if(re.search(regex,web)):   
                     st.write(web)  
+                    db.child("Web").push(web)
 
         for i in range(len(result_text)):
             check_url(result_text[i])
-    return image
-# def hist(img):
-#     st.image(img)
-
-# if options=='Main':
-#     main()
-# if options=='History':
-#     hist(img)
-
-
-def firebase_main():
-    st.title("Welcome!")
-
-    choice=st.selectbox('login/signup',('login','signup'))
-    # choice = 'login'
-
-    email = st.text_input("Email")
-    password = st.text_input("password",type='password')
-
-    if choice=='signup':
-        handle=st.text_input('Handle name',value='Default')
-        submit=st.button('Create my account')
-
-        if submit:
-            user=auth.create_user_with_email_and_password(email,password)
-            st.success('Account created') 
-            st.balloons()
-            user=auth.sign_in_with_email_and_password(email,password)
-            db.child(user['localID']).child("Handle").set(handle)
-            db.child(user['localID']).child("ID").set(user['localID'])
-            st.title("Welcome"+handle)
-
-    if choice=='login':
-        login=st.button('Continue')
-        if login:
-            user=auth.sign_in_with_email_and_password(email,password)
-            st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-            newImgPath=main_function()
-            uid = user['localId']
-            # Stored Initated Bucket in Firebase
-            fireb_upload = storage.child(uid).put(newImgPath,user['idToken'])
-            # Get the url for easy access
-            a_imgdata_url = storage.child(uid).get_url(fireb_upload['downloadTokens']) 
-            # Put it in our real time database
-            db.child(user['localId']).child("Image").push(a_imgdata_url)
             
 
-firebase_main()
+
+def hist():
+    col1, col2, col3 = st.columns((1,1,1))
+
+    with col1:
+        st.markdown('Email')
+        temp1 = db.child("Email").get().val()
+        for i,j in enumerate(temp1):
+            st.code(temp1[j])
+
+    with col2:
+        st.markdown('Number')
+        temp2 = db.child("Number").get().val()
+        for i,j in enumerate(temp2):
+            st.code(temp2[j])
+
+    with col3:
+        st.markdown('Web')
+        temp3 = db.child("Web").get().val()
+        for i,j in enumerate(temp3):
+            st.code(temp3[j])
+
+
+if options=='Main':
+    main_function()
+if options=='History':
+    hist()
 
 
